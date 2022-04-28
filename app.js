a1c873307b7e0979d2cbaa16dedba98f3c50d968
Nicolas Deciancio <nicolas.deciancio@WUST040014.bayer.cnb>
2022-04-28 00:18:12 -0300

    SPOT-28 initial gateway configuration

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 const createError = require('http-errors');
 const express = require('express');
+const port = process.env.PORT || 8080;
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const {ROUTES} = require("./routes/routes");
+const {setupRateLimit} = require("./ratelimit/ratelimit");
+const {setupProxies} = require("./proxy");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+setupRateLimit(app, ROUTES);
+setupProxies(app, ROUTES);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
+/*// error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -31,6 +31,8 @@ app.use((err, req, res, next) => {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});*/
 
-module.exports = app;
+app.listen(port, () => {
+  logger(`Server app listening at http://localhost:${port}`)
+})
