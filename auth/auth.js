11e4a49ab3f11f30781ac9da15e2e8d168a20773
Nicolas Deciancio <nicolas.deciancio@WUST040014.bayer.cnb>
2022-05-09 16:05:33 -0300

    fix status codes

--- auth/auth.js
+++ auth/auth.js
@@ -3,11 +3,11 @@ const firebase = require("./firebase/firebase");
 const authMiddleware = (req, res, next) => {
   const headerToken = req.headers.authorization;
   if (!headerToken) {
-    return res.send({ message: "No token provided" }).status(401);
+    return res.status(401).send({ message: "No token provided" }).end();
   }
 
   if (headerToken && headerToken.split(" ")[0] !== "Bearer") {
-    res.send({ message: "Invalid token" }).status(401);
+    return res.status(401).send({ message: "Invalid token" }).end();
   }
 
   const token = headerToken.split(" ")[1];
@@ -15,15 +15,15 @@ const authMiddleware = (req, res, next) => {
     .auth()
     .verifyIdToken(token)
     .then(() => next())
-    .catch(() => res.send({ message: "Could not authorize" }).status(403));
+    .catch(() => res.status(403).send({ message: "Could not authorize" }).end());
 }
 
 const setupAuth = (app, routes) => {
-  app.post('/validate-session', authMiddleware);
   routes.forEach(r => {
     if (r.auth) {
       app.use(r.url, authMiddleware);
     }
+    app.post('/validate-session', authMiddleware);
   })
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,14 @@
 const ROUTES = [
   {
     url: '/users-api/',
-    auth: false,
+    auth: true,
     creditCheck: false,
     rateLimit: {
       windowMs: 15 * 60 * 1000,
       max: 5
     },
     proxy: {
-      target: "https://spotifiuby-backend-users.herokuapp.com/",
+      target: "https://spotifiuby-backend-users.herokuapp.com",
       changeOrigin: true,
       pathRewrite: {
         [`^/users-api`]: '',
